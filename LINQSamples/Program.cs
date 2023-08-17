9453324be9d2c5ac8b3beba338b454f6cb5a4e6d
Serhat Tomak <serhattomak@gmail.com>
2023-08-17 10:39:38 +0300

    Where Queries Learning how to use Where queries.

--- LINQSamples/Program.cs
+++ LINQSamples/Program.cs
@@ -17,27 +17,44 @@ namespace LINQSamples
         {
             using (var db = new NorthwindContext())
             {
-                //var products = db.Products.ToList();
-                //var products = db.Products.Select(p => p.ProductName).ToList();
-                //var products = db.Products.Select(p => new {p.ProductName, p.UnitPrice}).ToList();
-                //var products = db.Products.Select(p => new ProductModel
-                //{
-                //    Name = p.ProductName,
-                //    Price = p.UnitPrice
-                //}).ToList();
+                //var products = db.Products.Where(p=>p.UnitPrice>18).ToList();
+                //var products = db.Products.Select(p=>new {p.ProductName, p.UnitPrice}).Where(p=>p.UnitPrice>18).ToList();
+                //var products = db.Products.Where(p => p.UnitPrice > 18 && p.UnitPrice<30).ToList();
+                //var products = db.Products.Where(p => p.CategoryId >= 1 && p.CategoryId <= 5).ToList();
+                //var products = db.Products.Where(p => p.CategoryId == 1 || p.CategoryId == 5).ToList();
+                //var products = db.Products.Where(p=>p.CategoryId==1).Select(p => new { p.ProductName, p.UnitPrice }).ToList();
+                //var products = db.Products.Where(i=>i.ProductName=="Chai").ToList();
+                var products = db.Products.Where(i=>i.ProductName.Contains("Ch")).ToList();
 
-                //var product = db.Products.First();
-                var product = db.Products.Select(p=>new {p.ProductName, p.UnitPrice}).FirstOrDefault();
-
-                Console.WriteLine(product.ProductName + ' ' + product.UnitPrice);
-
-                //foreach (var p in products)
-                //{
-                //    Console.WriteLine(p.Name + ' ' + p.Price);
-                //}
+                foreach (var p in products)
+                {
+                    Console.WriteLine(p.ProductName + ' ' + p.UnitPrice);
+                }
             }
 
             Console.ReadLine();
+        }
+
+        private static void Lesson1(NorthwindContext db)
+        {
+            //var products = db.Products.ToList();
+            //var products = db.Products.Select(p => p.ProductName).ToList();
+            //var products = db.Products.Select(p => new {p.ProductName, p.UnitPrice}).ToList();
+            //var products = db.Products.Select(p => new ProductModel
+            //{
+            //    Name = p.ProductName,
+            //    Price = p.UnitPrice
+            //}).ToList();
+
+            //var product = db.Products.First();
+            var product = db.Products.Select(p => new { p.ProductName, p.UnitPrice }).FirstOrDefault();
+
+            Console.WriteLine(product.ProductName + ' ' + product.UnitPrice);
+
+            //foreach (var p in products)
+            //{
+            //    Console.WriteLine(p.Name + ' ' + p.Price);
+            //}
         }
     }
 }
