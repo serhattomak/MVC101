7526bb55aca6d562fd12ed01a0969094cf07a7bf
Serhat Tomak <serhattomak@gmail.com>
2023-08-17 14:04:01 +0300

    Exercise: Select Queries Exercising the select queries with some examples.

--- LINQSamples/Program.cs
+++ LINQSamples/Program.cs
@@ -17,22 +17,94 @@ namespace LINQSamples
         {
             using (var db = new NorthwindContext())
             {
-                //var products = db.Products.Where(p=>p.UnitPrice>18).ToList();
-                //var products = db.Products.Select(p=>new {p.ProductName, p.UnitPrice}).Where(p=>p.UnitPrice>18).ToList();
-                //var products = db.Products.Where(p => p.UnitPrice > 18 && p.UnitPrice<30).ToList();
-                //var products = db.Products.Where(p => p.CategoryId >= 1 && p.CategoryId <= 5).ToList();
-                //var products = db.Products.Where(p => p.CategoryId == 1 || p.CategoryId == 5).ToList();
-                //var products = db.Products.Where(p=>p.CategoryId==1).Select(p => new { p.ProductName, p.UnitPrice }).ToList();
-                //var products = db.Products.Where(i=>i.ProductName=="Chai").ToList();
-                var products = db.Products.Where(i=>i.ProductName.Contains("Ch")).ToList();
+
+                //ALL CUSTOMERS' INFO
+
+                //var customers = db.Customers.ToList();
+
+                //foreach (var c in customers)
+                //{
+                //    Console.WriteLine(c.ContactName);
+                //}
+
+                //ALL CUSTOMERS' ID
+
+                //var customerId = db.Customers.Select(c => new { c.CustomerId, c.ContactName }).ToList();
+
+                //foreach (var c in customerId)
+                //{
+                //    Console.WriteLine(c.CustomerId + ' ' + c.ContactName);
+                //}
+
+                //CUSTOMERS WHO LIVE IN GERMANY
+
+                //var customerLocation = db.Customers.Select(c=> new { c.ContactName, c.Country }).Where(c => c.Country == "Germany").ToList();
+
+                //foreach (var c in customerLocation)
+                //{
+                //    Console.WriteLine(c.Country + ' ' + c.ContactName);
+                //}
+
+                //WHERE DOES "DIEGO ROEL" LIVE?
+
+                //var locationDiego = db.Customers.Where(l => l.ContactName == "Diego Roel").FirstOrDefault();
+
+                //Console.WriteLine(locationDiego.ContactName + " " + locationDiego.CompanyName);
+
+                //WHICH PRODUCTS HAVE 0 UNITS IN STOCK
+
+                //var products = db.Products.Select(p=> new{ p.ProductName, p.UnitsInStock }).Where(p => p.UnitsInStock == 0).ToList();
+
+                //foreach (var p in products)
+                //{
+                //    Console.WriteLine(p.ProductName + " " + p.UnitsInStock);
+                //}
+
+                //ALL EMPLOYEES' FIRST AND LAST NAME
+
+                //var employees = db.Employees.Select(e => new { FullName = e.FirstName + ' ' + e.LastName });
+                //foreach (var e in employees)
+                //{
+                //    Console.WriteLine(e.FullName);
+                //}
+
+                //TAKE STARTING 5 VALUES
+
+                //var products = db.Products.Take(5).ToList();
+
+                //foreach (var p in products)
+                //{
+                //    Console.WriteLine(p.ProductName + " " + p.ProductId);
+                //}
+
+                //TAKE SECOND 5 VALUES
+
+                var products = db.Products.Skip(5).Take(5).ToList();
 
                 foreach (var p in products)
                 {
-                    Console.WriteLine(p.ProductName + ' ' + p.UnitPrice);
+                    Console.WriteLine(p.ProductName + " " + p.ProductId);
                 }
             }
 
             Console.ReadLine();
+        }
+
+        private static void Lesson2(NorthwindContext db)
+        {
+            //var products = db.Products.Where(p=>p.UnitPrice>18).ToList();
+            //var products = db.Products.Select(p=>new {p.ProductName, p.UnitPrice}).Where(p=>p.UnitPrice>18).ToList();
+            //var products = db.Products.Where(p => p.UnitPrice > 18 && p.UnitPrice<30).ToList();
+            //var products = db.Products.Where(p => p.CategoryId >= 1 && p.CategoryId <= 5).ToList();
+            //var products = db.Products.Where(p => p.CategoryId == 1 || p.CategoryId == 5).ToList();
+            //var products = db.Products.Where(p=>p.CategoryId==1).Select(p => new { p.ProductName, p.UnitPrice }).ToList();
+            //var products = db.Products.Where(i=>i.ProductName=="Chai").ToList();
+            var products = db.Products.Where(i => i.ProductName.Contains("Ch")).ToList();
+
+            foreach (var p in products)
+            {
+                Console.WriteLine(p.ProductName + ' ' + p.UnitPrice);
+            }
         }
 
         private static void Lesson1(NorthwindContext db)
