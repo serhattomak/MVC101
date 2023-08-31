61afee3ff9f9ea853cc64f262740854a213a40ee
Serhat Tomak <serhattomak@gmail.com>
2023-08-31 09:27:02 +0300

    Movie Update Page Creating movie update page on the Admin Movies section.

--- MovieApp/Controllers/AdminController.cs
+++ MovieApp/Controllers/AdminController.cs
@@ -11,18 +11,60 @@ namespace MovieApp.Controllers
 
         public AdminController(MovieContext context)
         {
-            _context=context;
+            _context = context;
         }
         public IActionResult Index()
         {
             return View();
         }
 
+        public IActionResult MovieUpdate(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entity = _context.Movies.Select(m => new AdminEditMovieViewModel()
+            {
+                MovieId = m.MovieId,
+                Title = m.Title,
+                Description = m.Description,
+                ImageUrl = m.ImageUrl
+            }).FirstOrDefault(m => m.MovieId == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult MovieUpdate(AdminEditMovieViewModel model)
+        {
+            var entity = _context.Movies.Find(model.MovieId);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            entity.Title=model.Title;
+            entity.Description = model.Description;
+            entity.ImageUrl=model.ImageUrl;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("MovieList");
+        }
+
         public IActionResult MovieList()
         {
             return View(new AdminMoviesViewModel()
             {
-                Movies = _context.Movies.Include(m=>m.Genres).Select(m=> new AdminMovieViewModel()
+                Movies = _context.Movies.Include(m => m.Genres).Select(m => new AdminMovieViewModel()
                 {
                     MovieId = m.MovieId,
                     Title = m.Title,
--- MovieApp/Models/AdminMoviesViewModel.cs
+++ MovieApp/Models/AdminMoviesViewModel.cs
@@ -14,4 +14,12 @@ namespace MovieApp.Models
         public string ImageUrl { get; set; }
         public List<Genre> Genres { get; set; }
     }
+
+    public class AdminEditMovieViewModel
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
 }
